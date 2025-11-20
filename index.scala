ea724efcd9f349b231baadf6e155aed233e0f795
Windymelt <windymelt@3qe.us>
2025-11-20 22:20:44 +0900

    parameter

--- index.scala
+++ index.scala
@@ -1,15 +1,25 @@
 //> using platform js
 //> using scala 3.7
 
+import scala.scalajs.js
 import scala.scalajs.js.annotation.*
+
+@js.native
+trait Event extends js.Object {
+  val name: String = js.native
+}
+
+@js.native
+trait Context extends js.Object
 
 object Index {
   @JSExportTopLevel(name = "handler", moduleID = "index")
-  def handler(): Unit = {
-    println("Hello world!")
+  def handler(event: Event, context: Context): Unit = {
+    println(s"Hello, ${event.name}!")
   }
 }
 
+// This object is just a dummy to package module
 // https://github.com/VirtusLab/scala-cli/issues/3964
 object Hello {
   def main(args: Array[String]): Unit = 42
